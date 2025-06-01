9990895c19a19277e245a23aa45a9d0253d5b142
Ediga Idu <iduedigabarnabas@gmail.com>
2025-06-01 21:19:45 +0100

    Initial commit

---
+++ 2-arguments.js
@@ -0,0 +1,5 @@
+const args = Process.argv.slice(2);
+if (args.lenght === 0) {console.log("No argument");
+} else if (args.lenght === 1) {console.log("Argument found");
+} else {console.log("Arguments found");
+}
