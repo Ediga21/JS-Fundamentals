4f6bea71fadbe130dbc6613ffe2cbc05fb771403
Ediga Idu <iduedigabarnabas@gmail.com>
2025-06-01 23:22:56 +0100

    Add 2-arguments.js script

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,9 @@
-const args = Process.argv.slice(2);
-if (args.lenght === 0) {console.log("No argument");
-} else if (args.lenght === 1) {console.log("Argument found");
-} else {console.log("Arguments found");
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
